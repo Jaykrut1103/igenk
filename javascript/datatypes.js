cab8eb1b5cacaa364253748a2a86291572690f96
jaykrut <jaykrut.kotadiya@ignek.com>
2024-11-06 11:59:12 +0530

    javascript data type concept completed with example

---
+++ javascript/datatypes.js
@@ -0,0 +1,11 @@
+const a=10;
+const obj={
+    name:"jaykrut",
+    age:"22"
+}
+const array=[10,20,30]
+const name="jaykrut"
+const booleanValue=true
+let student
+let student2=null
+console.log('student',student)
