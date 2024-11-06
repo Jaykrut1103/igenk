c7d500054cd8abe5543f62aa0fc9a4b2d894b2b0
jaykrut <jaykrut.kotadiya@ignek.com>
2024-11-06 11:38:06 +0530

    done the javascript operator concept with example

---
+++ javascript/operator.js
@@ -0,0 +1,16 @@
+// Assignment operator
+const a=9
+
+// Arithmentic operator
+const b=a+10
+
+// Comparison operator
+const c=a<b
+console.log("ans of c",c)
+
+// Logical operator
+const d =true && false
+console.log("ans of d",d)
+
+// Conditional operator
+a>10 ? console.log("a is greater than 10"):console.log("a is less than 10")
