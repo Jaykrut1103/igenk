14fd0bb7e5c88b86fc06105378bbbea348a7cba6
jaykrut <jaykrut.kotadiya@ignek.com>
2024-11-06 18:13:43 +0530

    completed block scope ans shadowing concept with example

---
+++ javascript/block_scope.js
@@ -0,0 +1,10 @@
+if(true){
+   let  a=10
+    console.log("a",a)
+}
+if(true){
+    var  b=10
+     console.log("b",b)
+ }
+ console.log("b",b)
+console.log("a",a)
---
+++ javascript/shadowing.js
@@ -0,0 +1,9 @@
+let x = 100;
+
+function test() {
+  let x = 50; 
+  console.log(x); 
+}
+
+test();
+console.log(x); 
